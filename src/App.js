89656655e4e7aeb5b29368fc2af1e0444812680c
jaygriggsau <110772005+jaygriggsau@users.noreply.github.com>
2023-01-17 04:41:25 +0000

    Foundational MVP

--- src/App.js
+++ src/App.js
@@ -1,29 +1,73 @@
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
 
-function App() {
+const { Configuration, OpenAIApi } = require("openai");
+
+const App = () => {
+  const [experience, setExperience] = useState('');
+  const [skills, setSkills] = useState('');
+  const [jobDesc, setJobDesc] = useState('');
+  const [result, setResult] = useState('');
+  const [error, setError] = useState('');
+
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  });
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const coverLetterPrompt = "Write a cover letter for the following job: "
+    try {
+      const openai = new OpenAIApi(configuration);
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: coverLetterPrompt + jobDesc + ". Link my skills to the job description. My skills are: " + skills + "and my experience is: " + experience,
+        temperature: 0.7,
+        max_tokens: 1555,
+      });
+      setResult(response.data.choices[0].text);
+    } catch (err) {
+      setError(err.toString());
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <Header />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Job Description:
+          <input
+            type="text"
+            value={jobDesc}
+            onChange={e => setJobDesc(e.target.value)}
+          />
+        </label>
+        <label>
+          Your Skills:
+          <input
+            type="text"
+            value={skills}
+            onChange={e => setSkills(e.target.value)}
+          />
+        </label>
+        <label>
+          Your Experience:
+          <input
+            type="text"
+            value={experience}
+            onChange={e => setExperience(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <section>
+        {error && <p>Error: {error}</p>}
+        {result && <p>Result: {result}</p>}
+      </section>
+      
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Header() {
+
+    return (
+        <section>
+            <h1>Cover Letter Generator</h1>
+            <p>Create a quick cover letter. Make sure to check and edit the document for your purposes</p>
+        </section>
+        
+    )
+}
